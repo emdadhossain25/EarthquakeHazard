0ea58e2b7f2c66d502ed3b7fd6a9fb85ae9bbe55
emdad.hossain25 <emdad.hossain321@gmail.com>
2017-05-06 02:28:26 +0600

    added dummy data for date and time

--- app/src/main/java/com/example/administrator/earthquakehazard/EarthQuakeAdapter.java
+++ app/src/main/java/com/example/administrator/earthquakehazard/EarthQuakeAdapter.java
@@ -12,7 +12,9 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.zip.Inflater;
 
 /**
@@ -42,7 +44,14 @@ public class EarthQuakeAdapter extends ArrayAdapter<EarthQuakeAdapterModel> {
         tv_location.setText(earthQuakeAdapterModel.getLocation());
 
         TextView tv_date_time = (TextView)listView.findViewById(R.id.tv_date_time);
-        tv_date_time.setText(earthQuakeAdapterModel.getDate_time()+"");
+        Long timeInmilliseconds = earthQuakeAdapterModel.getDate_time();
+        Date dateObject = new Date(timeInmilliseconds);
+        String dateToDisplay = formatDate(dateObject);
+        tv_date_time.setText(dateToDisplay);
+
+        TextView tv_time = (TextView)listView.findViewById(R.id.tv_time);
+        String time = formatTime(dateObject);
+        tv_time.setText(time);
 
         listView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +61,27 @@ public class EarthQuakeAdapter extends ArrayAdapter<EarthQuakeAdapterModel> {
         });
 
         return listView;
+    }
+
+
+    /**
+     * formating date into current date converting it from UNIX time
+     * @param dateObject
+     * @return
+     */
+    private String formatDate(Date dateObject){
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy");
+        return dateFormatter.format(dateObject);
+    }
+
+    /**
+     * formating time into current time converting it from UNIX time
+     * @param dateObject
+     * @return
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
     }
 }
 
--- app/src/main/java/com/example/administrator/earthquakehazard/EarthQuakeAdapterModel.java
+++ app/src/main/java/com/example/administrator/earthquakehazard/EarthQuakeAdapterModel.java
@@ -6,26 +6,27 @@ package com.example.administrator.earthquakehazard;
 
 public class EarthQuakeAdapterModel {
 
-        private Double  magnitude;
-        private String location;
-        private int date_time;
+    private Double magnitude;
+    private String location;
+    private Long date_time;
 
-        public EarthQuakeAdapterModel(Double magnitude, String location, int date_time) {
-            this.magnitude = magnitude;
-            this.location = location;
-            this.date_time = date_time;
+    public EarthQuakeAdapterModel(Double magnitude, String location, Long date_time) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date_time = date_time;
 
-        }
+    }
 
-        public Double getMagnitude() {
-            return magnitude;
-        }
+    public Double getMagnitude() {
+        return magnitude;
+    }
 
-        public String getLocation() {
-            return location;
-        }
+    public String getLocation() {
+        return location;
+    }
 
-        public int getDate_time() {
-            return date_time;
-        }
+    public Long getDate_time() {
+        return date_time;
+    }
+
 }
